cd5a9da4bd40a0ef54e4b55fc06e564ccaa60976
Aamir Sahil <notifysahil@gmail.com>
2025-05-14 00:07:21 +0530

    added game class

--- src/war_of_the_ring/config.py
+++ src/war_of_the_ring/config.py
@@ -1,4 +1,5 @@
 from model import DisplayConfig, GameConfig
+from war_of_the_ring.game import Game
 
 display_data = {
         "width" : 800,
@@ -8,9 +9,12 @@ display_data = {
     }
 display_config = DisplayConfig(**display_data)
 
+game = Game()
+
 game_data = {
     "title" : "War of the Ring",
-    "fps" : 60, 
+    "fps" : 60,
+    "game" : game,
     "display_config" : display_config
 }
 game_config = GameConfig(**game_data)
