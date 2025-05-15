344a23f273fbb483c74c5aa18befe3f0299cfb61
Aamir Sahil <notifysahil@gmail.com>
2025-05-15 13:29:06 +0530

    input manager class responds to events

---
+++ src/games/wotr/input_manager/input_manager.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from interface_api import IEventTypes
+
+class InputManager:
+    def __init__(self):
+        pass
+    def process_event(self, event : Optional[IEventTypes]):
+        if event == IEventTypes.Select:
+            print("Mouse Click")
+        elif event == IEventTypes.ScrollIn:
+            print("Scroll In")
+        elif event == IEventTypes.ScrollOut:
+            print("Scroll out")
+        elif event == IEventTypes.Dragging:
+            print("Dragging")
+        elif event == IEventTypes.Option:
+            print("Option")
