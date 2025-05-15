802d2b7c5dce816cb137f007a33b8e9d718d619d
Aamir Sahil <notifysahil@gmail.com>
2025-05-15 13:23:54 +0530

    sending game class directly to manager

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from war_of_the_ring import game_config, create_game
+from games import create_game, create_manager
 
 def main():
     game = create_game()
-    game.run()
+    manager = create_manager(game = game)
+    
+    manager.run()
     print("Done, Thanks for playing")
 
 if __name__=="__main__":
