94e89c38eb84b655911ae31acbacde48066a2f1c
Aamir Sahil <notifysahil@gmail.com>
2025-05-15 13:24:40 +0530

    display class have load, unload, draw and draw_all methods. added doc string aswell

--- src/core/display/display.py
+++ src/core/display/display.py
@@ -1,29 +1,74 @@
 import pygame
+from typing import Dict
 
-from model import DisplayConfig, DrawSurface
+from core.model import DisplayConfig, DrawSurface
 
-from core.display.scroll import Scroll
-from core.display.zoom import Zoom
+from interface_api import IDisplay, IDrawGraphics
 
-from typing import List, Dict
+class Display(IDisplay):
+    """
+    Handles screen related logic.
 
-class Display:
+    Attributes:
+        width (int) : screen width
+        height (int) : screen height
+        screen (pygame.Surface) : pygame screen element
+        surfaces (Dict[str , DrawSurface]) : surfaces to draw
+
+    Methods:
+        - load(surface: IDrawGraphics): Add surface to self.surfaces.
+        - unload(id: str): Remove element from self.surfaces.
+        - draw(id: str): Draw surface from surfaces by referencing id.
+        - draw_all(): Draw all elements in surfaces.
+    """
     def __init__(self, config : DisplayConfig):
-        self.WIDTH = config.width
-        self.HEIGHT = config.height
+        self.width = config.width
+        self.height = config.height
         flag = pygame.FULLSCREEN if config.fullscreen else 0
-        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), flag, config.screen_depth)
-        # self.map = Map()
-        # self.scroll = Scroll()
-        # self.zoom = Zoom()
+        self.screen = pygame.display.set_mode((self.width, self.height), flag, config.screen_depth)
+        self.surfaces : Dict[str , DrawSurface] = {}
 
-    def draw(self, draw_element : List[DrawSurface]):
-        for element in draw_element:
-            self._draw_surface(element = element)
-
-    def _draw_surface(self, element : DrawSurface):
-        self.screen.blit(
-            source = element.surface,
-            dest = element.pos_offset,
-            area = element.view_rect
+    def load(self, surface : IDrawGraphics):
+        """
+        Add surface data to self.sufaces
+        Parameters:
+            surface (IDrawGraphics) : surface data sent by game module
+        """
+        surface = pygame.image.load(surface.asset_loc)
+        offset = surface.pos_offset
+        view_rect = pygame.Rect(*surface.view_area)
+        
+        draw_surface = DrawSurface(
+            surface = surface,
+            pos_offset = offset,
+            view_rect = view_rect
         )
+        self.surfaces[surface.id] = draw_surface
+    
+    def unload(self, id : str):
+        """
+        Remove surface from self.surfaces
+        Parameters:
+            id (str) : surface id to remove
+        """
+        if id in self.surfaces:
+            del self.surfaces[id]
+
+    def draw(self, id : str):
+        """
+        Draw the surface[id] on self.screen
+        Parameters:
+            id (str) : surface id to draw
+        """
+        self.screen.blit(
+            source = self.surfaces[id].surface,
+            dest = self.surfaces[id].pos_offset,
+            area = self.surfaces[id].view_rect
+        )
+
+    def draw_all(self):
+        """
+        Draw all surfaces in self.surfaces
+        """
+        for id,_ in self.surfaces.items():
+            self.draw(id = id)
