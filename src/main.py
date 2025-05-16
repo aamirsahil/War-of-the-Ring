befff035ce3c7e9f5d005e9a55220bcb92230f12
Aamir Sahil <notifysahil@gmail.com>
2025-05-16 09:43:46 +0530

    testing application workflow

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from games import create_game, create_manager
 
 def main():
     game = create_game()
-    # manager = create_manager(game = game)
+    manager = create_manager(game = game)
     
-    # manager.run()
+    manager.run()
     print("Done, Thanks for playing")
 
 if __name__=="__main__":
